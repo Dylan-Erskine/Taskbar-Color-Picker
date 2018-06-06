2cc109603355f49fdc8a19e03307bcb1469f938f
Dylan <dylan@digitalstock.co.nz>
2018-06-07 00:51:54 +1200

    added an installer fixed bug, now runs on startup, added settings page

--- TaskBarColorPicker/Properties/Resources.Designer.cs
+++ TaskBarColorPicker/Properties/Resources.Designer.cs
@@ -69,7 +69,19 @@ namespace TaskbarColorPicker.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
-        
+
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon main
+        {
+            get
+            {
+                object obj = ResourceManager.GetObject("main", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
